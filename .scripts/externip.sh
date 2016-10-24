9cced210bbeed07d50735a5eca4b6814ceab3be3
Darc <darc@darcfx.com>
2016-10-24 16:52:59 -0400

    Commit scripts folder

---
+++ .scripts/externip.sh
@@ -0,0 +1,14 @@
+#curl ifconfig.me
+dig +short myip.opendns.com @resolver1.opendns.com
+#curl ifconfig.me/ip
+#curl ifconfig.me/host
+#curl ifconfig.me/ua
+#curl ifconfig.me/port
+#curl ifconfig.me/lang
+#curl ifconfig.me/keepalive
+#curl ifconfig.me/connection
+#curl ifconfig.me/encoding
+#curl ifconfig.me/mime
+#curl ifconfig.me/charset
+#curl ifconfig.me/via
+#curl ifconfig.me/forwarded
